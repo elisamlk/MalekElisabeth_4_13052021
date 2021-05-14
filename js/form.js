6b25c388f465df1f8ef45a5d6264f720c3761e42
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-05-14 15:02:57 +0200

    Fonction vérification du champ nom

--- js/form.js
+++ js/form.js
@@ -4,8 +4,13 @@ let regex = /^[A-Za-z0-9]{1,}/;
 let firstName = document.getElementById("first");
 let firstError = document.querySelector(".first-error");
 let lastName = document.getElementById("last");
+let lastError = document.querySelector(".last-error");
+let email = document.getElementById("email");
+let emailError = document.querySelector(".email-error");
+let birthdate = document.getElementById("birthdate");
+let birthdateError = document.querySelector(".birthdate-error");
 
-function checkInfo() {
+function checkFirstName() {
   if (firstName.value.trim() == "") {
     firstName.style.borderColor = "red";
     firstError.innerHTML =
@@ -15,8 +20,22 @@ function checkInfo() {
   return true;
 }
 
+function checkLastName() {
+  if (lastName.value.trim() == "") {
+    lastName.style.borderColor = "red";
+    lastError.innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    return false;
+  }
+  return true;
+}
+
+function checkEmail() {};
+
+function checkBirthdate(){};
+
 form.addEventListener("submit", function (e) {
-  if (checkInfo() == false) {
+  if (checkFirstName() == false != checkLastName() == false) {
     e.preventDefault();
   }
 });
