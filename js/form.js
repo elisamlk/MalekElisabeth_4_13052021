8fdee2ca50baa18af1dc0e173c213032419dec79
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-06-02 15:50:20 +0200

    Ajout d'une regex pour controler le format date de naissance

--- js/form.js
+++ js/form.js
@@ -56,7 +56,8 @@ function checkEmail() {
 function checkBirthdate() {
   let birthdate = document.getElementById("birthdate");
   let birthdateError = document.querySelector(".birthdate-error");
-  if (birthdate.value.trim() == "") {
+  let birthdateRegex = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$/g;
+  if (birthdate.value.trim() == "" || !birthdate.value.match(birthdateRegex)) {
     isNotValid(birthdate);
     birthdateError.innerHTML = "Veuillez renseigner votre date de naissance.";
     return false;
